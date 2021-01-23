5503a30c5dc0507b2696f2228b322488e46da3a9
Isaacjohnson <isaac.designws@gmail.com>
2021-01-23 17:47:03 -0600

    commit tree 1744b84ed184ff1bc0ccd37e2e77c369e1a1530d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,28 +1,32 @@
 def badge_maker(name)
-    return "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  greetings = [] 
+  names.each do |name| 
+    greetings <<  badge_maker(name)
   end
-  
-  def batch_badge_creator(names)
-    greetings = [] 
-    names.each do |name| 
-      greetings <<  badge_maker(name)
-    end
-    return greetings
+  return greetings
+end
+
+def assign_rooms(speakers)
+  greet = []
+  speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"}
+  return greet
   end
-  
-  def assign_rooms(speakers)
-    greet = []
-    speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"}
-    return greet
-    end
-  
-  def printer(attendees)
-    first_result = batch_badge_creator(attendees)
-    first_result.each do |result|
-      puts result 
-    end
-    result = assign_rooms(attendees)
-    result.each do |result|
-      puts result 
-    end
+
+def printer(attendees)
+  first_result = batch_badge_creator(attendees)
+  first_result.each do |result|
+    puts result 
   end
+  result = assign_rooms(attendees)
+  result.each do |result|
+    puts result 
+  end
+end
+
+
+
+
